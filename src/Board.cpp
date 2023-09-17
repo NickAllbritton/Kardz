a7ba9d3479f23037f0d9dbda05a03dbc48b37bb8
NickAllbritton <nallbritton@sycamores.indstate.edu>
2023-09-17 00:23:16 -0400

    wrote some stuff then deleted it

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,11 +4,8 @@ Board::Board()
 {
     cardTableImg.loadFromFile("./resources/card_table.png");
     table.setTexture(cardTableImg);
-
-    table.setPosition(sf::Vector2f(0.f, 0.f));
     width = table.getLocalBounds().width;
     height = table.getLocalBounds().height;
-
 }
 
 void Board::draw(sf::RenderWindow &wnd)
